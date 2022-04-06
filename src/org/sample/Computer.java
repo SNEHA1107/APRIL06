fb3b872bd78735bc516c5a53d36f974e525f1aa7
SNEHA1107 <snehakumaran11@gmail.com>
2022-04-06 13:48:54 +0530

    Few statements added

--- src/org/sample/Computer.java
+++ src/org/sample/Computer.java
@@ -3,5 +3,6 @@ package org.sample;
 public class Computer {
 	public void computerModel() {
 		System.out.println("Computer Model: Lenovo");
+		System.out.println("Computer window version:11");
 	}
 }
